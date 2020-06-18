8b0fda79025b3ad5c61ef5fbb6b4ea1d2aabb121
Kichiyaki <xyztojajestem@gmail.com>
2020-06-18 15:57:03 +0200

    add omitempty to ennoblement model json tag

--- models/ennoblement.go
+++ models/ennoblement.go
@@ -4,10 +4,10 @@ import "time"
 
 type Ennoblement struct {
 	VillageID  int       `gqlgen:"-" json:"villageID"`
-	Village    *Village  `gqlgen:"-" json:"village"`
+	Village    *Village  `gqlgen:"-" json:"village,omitempty"`
 	NewOwnerID int       `gqlgen:"-" json:"newOwnerID"`
-	NewOwner   *Player   `gqlgen:"-" json:"newOwner"`
+	NewOwner   *Player   `gqlgen:"-" json:"newOwner,omitempty"`
 	OldOwnerID int       `gqlgen:"-" json:"oldOwnerID"`
-	OldOwner   *Player   `gqlgen:"-" json:"oldOwner"`
+	OldOwner   *Player   `gqlgen:"-" json:"oldOwner,omitempty"`
 	EnnobledAt time.Time `json:"ennobledAt" gqlgen:"ennobledAt"`
 }
