9b5a94793865617f1f77285e790c0923cceb2008
Kichiyaki <xyztojajestem@gmail.com>
2020-06-18 16:51:11 +0200

    add coords filter to VillageFilter struct

--- models/village.go
+++ models/village.go
@@ -27,7 +27,7 @@ func (v *Village) Continent() string {
 }
 
 type VillageFilter struct {
-	tableName struct{} `urlstruct:"village"`
+	tableName struct{} `urlstruct:"village" json:"tableName" gqlgen:"tableName"`
 
 	ID    []int `json:"id" gqlgen:"id"`
 	IdNEQ []int `json:"idNEQ" gqlgen:"idNEQ"`
@@ -36,6 +36,15 @@ type VillageFilter struct {
 	NameNEQ   []string `json:"nameNEQ" gqlgen:"nameNEQ"`
 	NameMATCH string   `json:"nameMATCH" gqlgen:"nameMATCH"`
 	NameIEQ   string   `json:"nameIEQ" gqlgen:"nameIEQ"`
+
+	XGT  int `json:"xGT" gqlgen:"xGT"`
+	XGTE int `json:"xGTE" gqlgen:"xGTE"`
+	XLT  int `json:"xLT" gqlgen:"xLT"`
+	XLTE int `json:"xLTE" gqlgen:"xLTE"`
+	YGT  int `json:"yGT" gqlgen:"yGT"`
+	YGTE int `json:"yGTE" gqlgen:"yGTE"`
+	YLT  int `json:"yLT" gqlgen:"yLT"`
+	YLTE int `json:"yLTE" gqlgen:"yLTE"`
 
 	Points    int `json:"points" gqlgen:"points"`
 	PointsGT  int `json:"pointsGT" gqlgen:"pointsGT"`
