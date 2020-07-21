9ea33a7329904e66751245518acb196d78e0d615
Kichiyaki <xyztojajestem@gmail.com>
2020-07-21 11:47:28 +0200

    add new language tags

--- models/lang_version.go
+++ models/lang_version.go
@@ -13,13 +13,43 @@ const (
 	LanguageTagPL  LanguageTag = "pl"
 	LanguageTagInt LanguageTag = "en"
 	LanguageTagDE  LanguageTag = "de"
+	LanguageTagUK  LanguageTag = "uk"
+	LanguageTagIT  LanguageTag = "it"
+	LanguageTagFR  LanguageTag = "fr"
+	LanguageTagUS  LanguageTag = "us"
+	LanguageTagNL  LanguageTag = "nl"
+	LanguageTagES  LanguageTag = "es"
+	LanguageTagRO  LanguageTag = "ro"
+	LanguageTagRU  LanguageTag = "ru"
+	LanguageTagGR  LanguageTag = "gr"
+	LanguageTagTR  LanguageTag = "tr"
+	LanguageTagCS  LanguageTag = "cs"
+	LanguageTagCH  LanguageTag = "ch"
+	LanguageTagPT  LanguageTag = "pt"
+	LanguageTagBR  LanguageTag = "br"
+	LanguageTagHU  LanguageTag = "hu"
 )
 
 func (lt LanguageTag) IsValid() bool {
 	switch lt {
 	case LanguageTagPL,
 		LanguageTagInt,
-		LanguageTagDE:
+		LanguageTagDE,
+		LanguageTagBR,
+		LanguageTagCH,
+		LanguageTagCS,
+		LanguageTagES,
+		LanguageTagFR,
+		LanguageTagGR,
+		LanguageTagHU,
+		LanguageTagIT,
+		LanguageTagNL,
+		LanguageTagPT,
+		LanguageTagRO,
+		LanguageTagRU,
+		LanguageTagTR,
+		LanguageTagUK,
+		LanguageTagUS:
 		return true
 	}
 	return false
