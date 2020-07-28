e9db0f1975151a2b6ff1d067658d58431e40558a
Kichiyaki <xyztojajestem@gmail.com>
2020-07-28 12:24:44 +0200

    add cache/allservers package

---
+++ cache/allservers/allservers.go
@@ -0,0 +1,43 @@
+package allservers
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/pkg/errors"
+	"github.com/tribalwarshelp/shared/models"
+)
+
+const (
+	key = "all_servers"
+)
+
+type AllServersCache interface {
+	Get() ([]*models.Server, bool)
+	Clear() error
+}
+
+type allServersCache struct {
+	client redis.UniversalClient
+}
+
+func (c *allServersCache) Get() ([]*models.Server, bool) {
+	sJSON, err := c.client.Get(context.Background(), key).Result()
+	if sJSON == "" || err != nil {
+		return []*models.Server{}, false
+	}
+
+	servers := []*models.Server{}
+	if err := json.Unmarshal([]byte(sJSON), &servers); err != nil {
+		return []*models.Server{}, false
+	}
+	return servers, true
+}
+
+func (c *allServersCache) Clear() error {
+	if err := c.client.Del(context.Background(), key).Err(); err != nil {
+		return errors.Wrap(err, "All servers cache")
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/tribalwarshelp/shared
 
 go 1.14
+
+require (
+	github.com/go-redis/redis/v8 v8.0.0-beta.7
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/otel v0.9.0 // indirect
+)
